cae8201a679ae6e045f52fc8e66ad18003436175
Ari <arashkermanikolankeh@Arashs-MBP.local>
2022-03-01 22:10:49 +0100

    some corrections in docker settings

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from app import app
 
 if __name__ == "__main__":
-    print("App is running on port http://127.0.0.1:8000/")
+    print("App is running")
     
-    app.run(debug=True,host='0.0.0.0') #, port=8000
+    app.run(debug=True,host='0.0.0.0', port=8000) #
