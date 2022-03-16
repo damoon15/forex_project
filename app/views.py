229bad8aba0b1f005d84d03979147ee4cda26b82
Ari <arashkermanikolankeh@Arashs-MBP.local>
2022-03-16 12:59:06 +0100

    some improvements

--- app/views.py
+++ app/views.py
@@ -18,7 +18,6 @@ def home():
     return render_template('predictions.html', graphs= list)
 
 
-#@app.route('/', methods=['POST'])
 @app.route('/build_plot')
 def build_plot():
     matplotlib.use("agg")
@@ -30,9 +29,9 @@ def build_plot():
     predictions, residual, error_percent = get_prediction_res(model, test_data)
 
     #%%model_performance:
-    fig1, ax1 = plt.subplots(figsize=(10,4))
+    fig1, ax1 = plt.subplots(figsize=(12,6))
     x_t = test_data.index
-    x_t_str = [str(i) for i in x_t]
+    x_t_str = [str(i)[:-8] for i in x_t]
     ax1.plot(x_t, test_data, linestyle='dashed', marker='s')
     ax1.plot(x_t, predictions, linestyle='dashed', marker='s')
     label0 = "error = {:.2f}%".format(abs(error_percent[0]))
@@ -41,28 +40,32 @@ def build_plot():
         label = "error = {:.2f}%".format(abs(error_percent[i]))
         ax1.annotate(label, xy=(mdates.date2num(x_t[i])-0.27, predictions[i]-0.00002), xycoords='data')
     ax1.set_xticks(x_t)
-    ax1.set_xticklabels(x_t_str)
+    ax1.set_xticklabels(x_t_str, rotation=15)
     ax1.legend(('Data', 'Predictions'), fontsize=16)
     ax1.set_title('Japanese Yen/Australian Dollar', fontsize=20)
     ax1.set_ylabel('Close Rate', fontsize=16)
+    ax1.set_xlabel('Date', fontsize=14)
     fig1.savefig(img, format='png')
     img.seek(0)
     #%% peaks:
     img1 = io.BytesIO()
-    seasonal_prediction = get_prediction(model, train_data.index[0], date.today()+timedelta(days=3))
+    seasonal_prediction = get_prediction(model, train_data.index[0], date.today()+timedelta(days=14))
     seasonal = get_seasonal(seasonal_prediction)
-    df = get_peaks(seasonal, n=3)
-    df_plot = df.loc[date.today()-timedelta(days=15):]
-    fig2 , ax2 = plt.subplots()
+    df = get_peaks(seasonal, n=2)
+    df_plot = df.loc[date.today()-timedelta(days=30):]
+    fig2 , ax2 = plt.subplots(figsize=(12,6))
     ax2.scatter(df_plot.index, df_plot['min'], c='r')
     ax2.scatter(df_plot.index, df_plot['max'], c='g')
-    ax2.plot(df_plot.index, df_plot['data'])
+    #ax2.plot(df_plot.index, df_plot['data'])
+    ax2.plot(df_plot.index, df_plot['data'], linestyle='dashed')
     #ax2.set_xticks(rotation=90)
     fig2.autofmt_xdate(rotation=45)
-    #ax2.plot([str(i) for i in df_plot.index], df_plot['data'], linestyle='dashed', marker='s')
-    ax2.set_title('Seasonal peaks of the predicted data', fontsize=20)
-    ax2.set_ylabel('Close Rate', fontsize=16)
-    ax2.set_xlabel('Date', fontsize=16)
+    #label_peak = "close_rate = {:.5f}".format(seasonal_prediction[-4])+"\n"+"peak_date={}".format(df_plot.index[-4].date())
+    bbox_props = dict(boxstyle="round,pad=0.5", fc="w", ec="k", lw=2)
+
+    ax2.set_title('Seasonal peaks of the predicted data', fontsize=14)
+    ax2.set_ylabel('Seasonal Close Rate', fontsize=16)
+    ax2.set_xlabel('Date', fontsize=14)
     fig2.savefig(img1, format='png')
     img1.seek(0)
     #%% future prediction
@@ -73,48 +76,62 @@ def build_plot():
     future_predict = get_prediction(model, date.today(), date.today() + timedelta(days=3))
     x = future_predict.index
     x_dates = mdates.date2num([x])
-    x_str = [str(i) for i in x]
+    x_str = [str(i)[:-8] for i in x]
     #ax3.plot(x_str, future_predict, linestyle='dashed', marker='s')
     ax3.plot(x, future_predict, linestyle='dashed', marker='s')
     ax3.set_xticks(x)
     ax3.set_xticklabels(x_str)
     #for i in range(len(x)):
+    '''
     label0 = "close_rate = {:.6f}".format(future_predict[0])
-    ax3.annotate(label0, xy=(mdates.date2num(x[0]), future_predict[0]+0.000003), xycoords='data')
+    ax3.annotate(label0, xy=(mdates.date2num(x[0])+0.05, future_predict[0]), xycoords='data', fontsize=12)
     label1 = "close_rate = {:.6f}".format(future_predict[1])
-    ax3.annotate(label1, xy=(mdates.date2num(x[1])+0.05, future_predict[1]), xycoords='data')
+    ax3.annotate(label1, xy=(mdates.date2num(x[1])+0.05, future_predict[1]), xycoords='data', fontsize=12)
     label2 = "close_rate = {:.6f}".format(future_predict[2])
-    ax3.annotate(label2, xy=(mdates.date2num(x[2])+0.05, future_predict[2]), xycoords='data')
+    ax3.annotate(label2, xy=(mdates.date2num(x[2])+0.05, future_predict[2]), xycoords='data', fontsize=12)
     label3 = "close_rate = {:.6f}".format(future_predict[3])
-    ax3.annotate(label3, xy=(mdates.date2num(x[3])-0.6, future_predict[3]), xycoords='data')
+    ax3.annotate(label3, xy=(mdates.date2num(x[3])-0.7, future_predict[3]), xycoords='data', fontsize=12)
+    '''
 
-
-    #ax3.plot(future_predict)
-    #for i, j in zip(x_dates, future_predict):
-
-    #    label = "{:.2f}".format(j)
-    #ax3.text(x_dates[0], future_predict[0], "salam", color='red')
-    #ax3.annotate('Test', xy=(mdates.date2num(x[1]), future_predict[1]), xycoords='data')
-    #fig3.autofmt_xdate()
-    #ax3.text(, future_predict[i], "close = {}".format(future_predict[i]))
-    #ax3.plot(future_predict)
-    ax3.set_title('Future prediction for Japanese Yen/Australian Dollar', fontsize=20)
+    ax3.set_title('Future prediction for Japanese Yen/Australian Dollar', fontsize=14)
     ax3.set_ylabel('Close Rate', fontsize=16)
+    ax3.set_xlabel('Date', fontsize=14)
     fig3.savefig(img2, format='png')
     img2.seek(0)
+    #%%
+    img4 = io.BytesIO()
+    total_prediction = get_prediction(model, train_data.index[0], date.today()+timedelta(days=7))
+    #seasonal = get_seasonal(seasonal_prediction)
+    df = get_peaks(total_prediction, n=2)
+    df_plot = df.loc[date.today()-timedelta(days=30):]
+    fig4 , ax4 = plt.subplots(figsize=(12,6))
+    ax4.scatter(df_plot.index, df_plot['min'], c='r')
+    ax4.scatter(df_plot.index, df_plot['max'], c='g')
+    #ax2.plot(df_plot.index, df_plot['data'])
+    ax4.plot(df_plot.index, df_plot['data'], linestyle='dashed')
+    #ax2.set_xticks(rotation=90)
+    fig4.autofmt_xdate(rotation=45)
+
+    ax4.set_title('peaks of the predicted data', fontsize=14)
+    ax4.set_ylabel('Close Rate', fontsize=16)
+    ax4.set_xlabel('Date', fontsize=14)
+    fig4.savefig(img4, format='png')
+    img4.seek(0)
     #%% prepare for html
     plot_url = base64.b64encode(img.getvalue()).decode()
     plot_url1 = base64.b64encode(img1.getvalue()).decode()
     plot_url2 = base64.b64encode(img2.getvalue()).decode()
+    plot_url4 = base64.b64encode(img4.getvalue()).decode()
     out = "data:image/png;base64,{}".format(plot_url)
     out1 = "data:image/png;base64,{}".format(plot_url1)
     out2 = "data:image/png;base64,{}".format(plot_url2)
+    out4 = "data:image/png;base64,{}".format(plot_url4)
 
-    list = [out, out1, out2, "Model Performance:", "Forex prediction for the next 3 days:", "Seasonal maximums and minimums from the predictions:"]
+    list = [out, out1, out2, out4]
 
     return render_template('predictions.html', graphs=list)
+    #return render_template(graphs=list)
 
 
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
